74bc5ec6c133bcc4d2a3c4eed2c107d6958b8d79
GabrielIndrawan <Abelepic3@gmail.com>
2025-05-23 22:56:25 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        host: "localhost",
+        port: 5000,
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
+    })
+
+    server.route(routes);
+
+    await server.start();
+
+    console.log(`Server berjalan di ${server.info.uri}`);
+
+}
+
+init();
